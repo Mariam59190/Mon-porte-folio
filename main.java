6df487ad913cd228d7b8fec4d75ad3579fbdd1aa
mariam <mariamwalamako@gmail.com>
2026-05-09 00:06:24 +0200

    Ajout de fichiers et de codes

---
+++ main.java
@@ -0,0 +1,28 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        String nom = "Mariam Walamako Keita";
+        int age = 18;
+        String formation = "B1 Informatique";
+        String ecole = "Ynov Paris Campus";
+
+        System.out.println("===== PORTFOLIO JAVA =====");
+
+        System.out.println("Nom : " + nom);
+
+        System.out.println("Age : " + age + " ans");
+
+        System.out.println("Formation : " + formation);
+
+        System.out.println("École : " + ecole);
+
+        System.out.println("Compétences : HTML, CSS, SQL, JavaScript");
+
+        System.out.println("Projet : Création de portfolio web");
+
+        System.out.println("Merci de visiter mon portfolio.");
+
+    }
+
+}
